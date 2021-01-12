b5634cf8b31a488a7141972c4c5ff8a4c3cee33d
henriquelima12 <henrique99379414@gmail.com>
2021-01-12 17:18:51 -0300

    primeiro commit

---
+++ js/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+	{
+		titulo: 'VEREADOR',
+		numeros: 5,
+		candidatos: [
+			{
+				numero: '38111' ,
+				nome: 'Fulano de Tal',
+				partido: 'ABC',
+				fotos: [
+					{url: './img/vereador.jpg', legenda: 'Vereador'}
+				]
+			},
+
+			{
+				numero: '77222',
+				nome: 'Beltrano da Silva',
+				partido: 'DEFG',
+				fotos: [
+					{url: './img/vereador2.jpg', legenda: 'Vereador'}
+				]
+			},
+		]
+	},
+	{
+		titulo: 'PREFEITO',
+		numeros: 2,
+		candidatos: [
+			{
+				numero: '96',
+				nome: 'Ciclano',
+				partido: 'ABC',
+				vice: 'Cic',
+				fotos: [
+					{url: './img/prefeito.jpg', legenda: 'Prefeito'},
+					{url: './img/vice-prefeito.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+			},
+
+			{
+				numero: '84',
+				nome: 'Zulano',
+				partido: 'DEFG',
+				vice: 'Zul',
+				fotos: [
+					{url: './img/prefeito2.jpg', legenda: 'Prefeito'},
+					{url: './img/vice-prefeito2.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+			},
+		]
+	}
+
+];
